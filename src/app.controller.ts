68652b8a4a42984b6dcf133858d273eb31fe3464
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2021-05-08 12:23:07 -0500

    multiple parameter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,8 +10,16 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('products/:id')
-  public getProduct(@Param() params: string): string {
-    return params;
+  @Get('products/:productId')
+  public getProduct(@Param('productId') productId: string): string {
+    return `Product ${productId}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  public getCategories(
+    @Param('id') id: string,
+    @Param('productId') productId: string,
+  ): string {
+    return `Product ${productId} and ${id}`;
   }
 }
